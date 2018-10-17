548634f9c878524e4fb7fc5ece135574ed0f21f2
Hemendra Sharma <hemendra.sharma11@gmail.com>
2018-10-17 14:20:24 +0530

    Created a new Android Studio project

---
+++ app/src/main/java/hemendra/books/BooksActivity.kt
@@ -0,0 +1,12 @@
+package hemendra.books
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class BooksActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_books)
+    }
+}
